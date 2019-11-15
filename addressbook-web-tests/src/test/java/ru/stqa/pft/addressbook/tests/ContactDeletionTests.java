e45d48733286904d2e24949e750a5aee67bffef9
Elena Filiptsova <elena_filiptsova@relex.ru>
2019-11-15 18:24:18 +0300

    Правки по заданию 14

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -24,7 +24,7 @@ public class ContactDeletionTests extends TestBase {
         Contacts before = app.contact().all();
         ContactData deletedContact = before.iterator().next();
         app.contact().delete(deletedContact);
-        Contacts after = (Contacts) app.contact().all();
+        Contacts after = app.contact().all();
         assertEquals(after.size(), before.size()-1);
 
         assertThat(after, equalTo(before.withOut(deletedContact)));
