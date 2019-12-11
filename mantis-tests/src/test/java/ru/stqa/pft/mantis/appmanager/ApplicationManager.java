b50ad009f06001515b05b32453e59ce6a25279f3
Elena Filiptsova <elena_filiptsova@relex.ru>
2019-12-11 18:37:58 +0300

    Добавлено задание 19

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -22,6 +22,7 @@ public class ApplicationManager {
     private MailHelper mailHelper;
     private JamesHelper jamesHelper;
     private ChangeHelper changeHelper;
+    private SoapHelper soapHelper;
 
     public ApplicationManager(String browser){
         this.browser = browser;
@@ -96,6 +97,13 @@ public class ApplicationManager {
             jamesHelper = new JamesHelper(this);
         }
         return jamesHelper;
+    }
+
+    public SoapHelper soap(){
+        if (soapHelper == null){
+            soapHelper = new SoapHelper(this);
+        }
+        return soapHelper;
     }
 }
 
